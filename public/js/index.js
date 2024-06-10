177fc5e68de982b7339345f0ba6d00e806050584
somramnani <som.ramnani@gmail.com>
2024-06-10 10:57:55 -0400

    add random character function and display

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
+async function fetchData() {
+  try {
+    const response = await fetch("./data/characters.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
+// Function to display random data
+async function displayRandomData() {
+  const data = await fetchData();
+  if (data && data.length > 0) {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomItem = data[randomIndex];
+    const container = document.getElementById("data-container");
+    container.innerHTML = `
+      <img src = "${randomItem.image}">
+      <p>ID: ${randomItem.id}</p>
+      <p>Name: ${randomItem.name}</p>
+      <p>Origin: ${randomItem.placeOfOrigin.name}</p>
+      <p>Description: ${randomItem.description}</p>
+    `;
+  } else {
+    console.error("No data available");
+  }
+}
