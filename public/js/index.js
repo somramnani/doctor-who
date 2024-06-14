022b5e929e29263987ccec355fd1fd5a2c30e0a5
somramnani <som.ramnani@gmail.com>
2024-06-14 10:47:14 -0400

    add HTML continer and edit displayRandomData function

--- public/js/index.js
+++ public/js/index.js
@@ -8,16 +8,18 @@ async function fetchData() {
   }
 }
 
-// Function to display random data
 async function displayRandomData() {
   const data = await fetchData();
   if (data && data.length > 0) {
     const randomIndex = Math.floor(Math.random() * data.length);
     const randomItem = data[randomIndex];
     const container = document.getElementById("data-container");
+
+    console.log(randomItem.image);
     container.innerHTML = `
-      <img src = "${randomItem.image}">
+      <img src = ${randomItem.image} id = "characterImage">
       <p>ID: ${randomItem.id}</p>
+       <a href =${randomItem.image} >Image link </a>
       <p>Name: ${randomItem.name}</p>
       <p>Origin: ${randomItem.placeOfOrigin.name}</p>
       <p>Description: ${randomItem.description}</p>
