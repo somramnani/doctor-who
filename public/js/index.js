257bda13dbe69bc73d022c770a1445bcb93384da
somramnani <som.ramnani@gmail.com>
2024-06-17 11:08:52 -0400

    add handleError function

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,15 @@ async function fetchData() {
   }
 }
 
+async function handleError() {
+  console.log("image cannot be loaded");
+  const image = document.getElementById("characterImage");
+
+  console.log(image);
+  image.src =
+    "https://yt3.googleusercontent.com/vjYIgjn-2RABqfy8tWY6RG36dCCIKu9BdynPUXo7H6B1-WR9qEp8sb_zrgGnNfuIkMFRIe4IG-w=s900-c-k-c0x00ffffff-no-rj";
+}
+
 async function displayRandomData() {
   const data = await fetchData();
   if (data && data.length > 0) {
@@ -17,7 +26,9 @@ async function displayRandomData() {
 
     console.log(randomItem.image);
     container.innerHTML = `
-      <img src = ${randomItem.image} id = "characterImage">
+      <img src = ${
+        randomItem.image
+      } id = "characterImage" onerror = ${handleError()}>
       <p>Name: ${randomItem.name}</p>
       <p>Origin: ${randomItem.placeOfOrigin.name}</p>
       <p>Status: ${randomItem.status}</p>
@@ -27,3 +38,6 @@ async function displayRandomData() {
     console.error("No data available");
   }
 }
+document
+  .getElementById("submit-button")
+  .addEventListener("click", displayRandomData);
